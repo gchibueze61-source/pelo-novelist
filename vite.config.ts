400925affe9b83cbeecf87403b7f21cf04a8f134
gchibueze61-source <gchibueze61@gmail.com>
2026-07-05 18:42:29 +0100

    Fix build errors and configure redirects

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,3 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 });
-export default defineConfig({
-  plugins: [react()],
-  base: '/gchibueze61-source/pelo-novelist /', // ⚠️ Put the name of your GitHub repository here!
-})
